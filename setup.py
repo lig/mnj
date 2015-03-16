903914fede06ab1b3f110ca951b5695ecb7b5220
Serge Matveenko <smatveenko@ptsecurity.com>
2015-03-16 15:17:09 +0300

    Integrate pytest an setuptools.

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    # user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -11,6 +33,7 @@ setup(
     tests_require=[
         'pytest',
     ],
+    cmdclass={'test': PyTest},
     author='Serge Matveenko',
     author_email='s@matveenko.ru',
     description=(
