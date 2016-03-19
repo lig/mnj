66dca1660a2637d7a1b337f120918311d68ba880
Serge Matveenko <s@matveenko.ru>
2016-03-19 03:28:48 +0300

    Bump version to 0.2dev1.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.version_info[:2] < (3, 4):
 
 setup(
     name='mnj',
-    version='0.1.4dev',
+    version='0.2dev1',
     packages=find_packages(),
     install_requires=install_requires,
     tests_require=[
